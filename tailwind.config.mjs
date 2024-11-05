ccff6dd4e14293229bb2bd93057568a8ae532677
Jose <josigna.cp@gmail.com>
2024-11-04 21:07:20 -0300

    se agrega diseño paleta de colores, estructura de la pagina

---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				
+				"primary-100":"#D8C9E6",
+				"primary-200":"#baabc8",
+				"primary-300":"#776a84",
+				"accent-100":"#A3D2CA",
+				"accent-200":"#46726b",
+				"text-100":"#333333",
+				"text-200":"#5c5c5c",
+				"bg-100":"#E6F6F6",
+				"bg-200":"#dcecec",
+				"bg-300":"#b4c3c3",
+				  
+				
+			},
+		},
+	},
+	plugins: [],
+}
